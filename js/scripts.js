9aadb8241bae1623621e5333ec33ba31739e48a4
toekki <75297484+toekki@users.noreply.github.com>
2021-01-28 15:43:25 -0600

    2.2 lesson (partially through)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+alert("\"Howdy there, Folks!\"");
+console.log("\"Howdy there, Folks!\"")
+console.log("console log by itself is just text - as long as you use quotes")
+
 var age = 47;
 console.log(age);
 
@@ -23,3 +27,40 @@ console.log("Domo arigato, Mister Roboto... Domo!")//another single line comment
 /* for multi-line comments - note that comments
 are good for myself (espeoncially when I plan to come back
 to it later...) and for others */
+
+
+//--------- start of 2.2 ---------------------------------
+//--- Strings
+var text1 = 'Hello';
+var text2 = "Hello - should look the same as the previous Hello";
+console.log(text1)
+console.log(text2)
+
+var text1 = 'Hello,', text2 = "testing out 2 strings one after the other.";
+console.log(text1, text2)
+
+var random = "\"anything?\"";
+console.log(random)
+
+//--- Numbers
+var numberInteger1 = 123, numberInteger2 = -123;
+var numberFloat = 1.23;
+console.log(numberInteger1)
+console.log(numberInteger2)
+console.log(numberFloat)
+
+var numberInteger1 = 123, numberInteger2 = -123;
+var numberFloat = 1.23;
+console.log(numberInteger1, numberInteger2, numberFloat)
+
+//--- Booleans
+var booleanA = true;
+var booleanB = false;
+console.log(booleanA)
+console.log(booleanB)
+console.log(booleanA, booleanB)
+
+//--- Undefined & Null
+
+console.log(thisVariableDoesntExist)
+
