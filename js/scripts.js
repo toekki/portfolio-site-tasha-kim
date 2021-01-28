b11542c96c4c1d08b0f9561dd1772ef871f38758
toekki <75297484+toekki@users.noreply.github.com>
2021-01-28 00:32:59 -0600

    2.1 JavaScript task

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var age = 47;
+console.log(age);
+
+var title = 'Ignant';
+console.log(title)
+
+var favage = 26;
+console.log(favage)
+
+var favAge = 28;
+console.log(favAge)
+
+console.log("Domo arigato, Mister Roboto... Domo!")
