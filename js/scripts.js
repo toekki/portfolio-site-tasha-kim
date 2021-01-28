c9d2fca511c93a5e149092b7fba6804c9d8f51de
toekki <75297484+toekki@users.noreply.github.com>
2021-01-28 00:50:26 -0600

    2.1 math/comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var age = 47;
 console.log(age);
 
-var title = 'Ignant';
+var title = 'Jolly Green Giant';
 console.log(title)
 
 var favage = 26;
@@ -10,4 +10,16 @@ console.log(favage)
 var favAge = 28;
 console.log(favAge)
 
-console.log("Domo arigato, Mister Roboto... Domo!")
+var size = 100;
+var maxSize = (size * 4) + 15;
+var minSize = (size / 3) - 15 + (size * 2);
+console.log(size)
+console.log(maxSize)
+console.log(minSize)
+
+//single line comment
+console.log("Domo arigato, Mister Roboto... Domo!")//another single line comment
+
+/* for multi-line comments - note that comments
+are good for myself (espeoncially when I plan to come back
+to it later...) and for others */
